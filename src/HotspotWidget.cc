bafff07f536ce767a68bf665c6c579f37aa7f811
Guido de Jong <guidoj@users.sf.net>
2006-09-11 20:07:11 +0000

    Added hotspot widget

---
+++ src/HotspotWidget.cc
@@ -0,0 +1,66 @@
+/*
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at
+ * your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE, GOOD TITLE or
+ * NON INFRINGEMENT.  See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
+ * Copyright (C) 2005-2006  Guido de Jong <guidoj@users.sf.net>
+ */
+
+#include "HotspotWidget.h"
+#include "MousePointerManager.h"
+
+HotspotWidget::HotspotWidget(const int x, const int y, const int w, const int h, const int a)
+: ActiveWidget(x, y, w, h, a)
+{
+}
+
+HotspotWidget::~HotspotWidget()
+{
+}
+
+int
+HotspotWidget::GetAction() const
+{
+  return action;
+}
+
+void
+HotspotWidget::GenerateActionEvent(const int a)
+{
+  ActionEvent ae(a);
+  for (std::list<ActionEventListener *>::iterator it = actionListeners.begin(); it != actionListeners.end(); ++it) {
+    (*it)->ActionPerformed(ae);
+  }
+}
+
+void
+HotspotWidget::Focus(Video *video)
+{
+  video = video;
+}
+
+void
+HotspotWidget::LeftClick(const bool toggle)
+{
+  if (toggle) {
+    GenerateActionEvent(GetAction());
+  }
+}
+
+void
+HotspotWidget::RightClick(const bool toggle)
+{
+  if (toggle) {
+  }
+}
---
+++ src/HotspotWidget.h
@@ -0,0 +1,42 @@
+/*
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at
+ * your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE, GOOD TITLE or
+ * NON INFRINGEMENT.  See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
+ * Copyright (C) 2005-2006  Guido de Jong <guidoj@users.sf.net>
+ */
+
+#ifndef HOTSPOT_WIDGET_H
+#define HOTSPOT_WIDGET_H
+
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#include "Widget.h"
+
+class HotspotWidget
+: public ActiveWidget
+{
+  public:
+    HotspotWidget(const int x, const int y, const int w, const int h, const int a);
+    virtual ~HotspotWidget();
+    int GetAction() const;
+    void GenerateActionEvent(const int a);
+    void Focus(Video *video);
+    void LeftClick(const bool toggle);
+    void RightClick(const bool toggle);
+};
+
+#endif
