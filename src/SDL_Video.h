a18fc0068958b979428abb68eef6bfd80c17311d
Guido de Jong <guidoj@users.sf.net>
2010-01-01 13:35:06 +0000

    Updated license to GPL v3

--- src/SDL_Video.h
+++ src/SDL_Video.h
@@ -1,20 +1,20 @@
 /*
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or (at
- * your option) any later version.
+ * This file is part of xBaK.
  *
- * This program is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE, GOOD TITLE or
- * NON INFRINGEMENT.  See the GNU General Public License for more
- * details.
+ * xBaK is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * xBaK is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ * along with xBaK.  If not, see <http://www.gnu.org/licenses/>.
  *
- * Copyright (C) 2005-2009 Guido de Jong <guidoj@users.sf.net>
+ * Copyright (C) Guido de Jong <guidoj@users.sf.net>
  */
 
 #ifndef SDL_VIDEO_H
