4e5f85ad283104714377287a3b89e32d38e6215b
Cesar Canassa <cesar@canassa.com>
2026-07-29 20:32:04 +0200

    🧹 untrack repo-local scratchpad, gitignore it
    The scratchpad/ analysis scripts were swept into the previous commit by
    git add -A; they are throwaway and don't belong in the tree.

--- scratchpad/party.py
+++
@@ -1,5 +0,0 @@
-import re
-data=open('/home/canassa/src/github.com/canassa/bak2/viewer/data/STARTUP.GAM','rb').read()
-# find printable name strings near start (characterNames[6][10])
-for m in re.finditer(rb'[A-Z][a-z]{3,9}\x00', data[:0x900]):
-    print('%#06x: %s'%(m.start(), m.group().rstrip(b'\x00').decode()))
